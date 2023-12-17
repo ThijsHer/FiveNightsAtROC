0b7162b3163edfd4d3553c91b048bb3a5c800c20
smeikol <564500@edu.rocmn.nl>
2023-12-17 09:22:15 +0100

    reset button

--- Assets/quitgame.cs
+++ Assets/quitgame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class quitgame : MonoBehaviour
 {
@@ -20,4 +21,12 @@ public class quitgame : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void resetgame()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Mainmenu");
+    } 
+
+
 }
