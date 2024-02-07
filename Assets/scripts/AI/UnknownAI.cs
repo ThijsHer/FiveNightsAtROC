0423c4c721e5ce138d4dc3dd12ab6ab63f4c8fb5
Kaya <Dogubroz@gmail.com>
2024-02-07 17:18:16 +0100

    Night 3 bijna klaar (korte night daarom zo snel)

---
+++ Assets/scripts/AI/UnknownAI.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UnknownAI : MonoBehaviour
+{
+    private string currentlocation;
+
+    public int ailevel;
+
+    private int hallwindow;
+
+    public GameObject wait;
+    public GameObject Hidden;
+    public GameObject Cam1;
+    public GameObject Cam2;
+
+    public GameObject jumpscareobject;
+
+    public GameObject cam2static;
+    public GameObject cam1static;
+
+    public GameObject shitdatindewegzit;
+
+    public AudioSource gonegeluid;
+    public Canvas deurui;
+    public GameObject doorcam;
+    public Button backtoofficebutton;
+    public GameObject camerahandler;
+    public AudioSource jumpscare;
+
+    void Start()
+    {
+        currentlocation = "scriptedwait";
+        StartCoroutine(UnknownMove());
+    }
+
+    IEnumerator UnknownMove()
+    {
+        yield return new WaitForSeconds(5f);
+
+        int chance = UnityEngine.Random.Range(1, 21);
+        Debug.Log(chance);
+
+        if (currentlocation == "scriptedwait")
+        {
+            yield return new WaitForSeconds(1f);
+            wait.gameObject.SetActive(false);
+            currentlocation = "hidden";
+            Hidden.gameObject.SetActive(true);
+        }
+        else
+        {
+            if (chance <= ailevel || currentlocation == "hidden")
+            {
+                if (currentlocation == "hidden")
+                {
+                    Hidden.gameObject.SetActive(false);
+                    cam2static.SetActive(true);
+                    currentlocation = "cam2";
+                    Cam2.gameObject.SetActive(true);
+                    yield return new WaitForSeconds(0.1f);
+                    cam2static.SetActive(false);
+                }
+                else if (currentlocation == "cam2")
+                {
+                    cam2static.SetActive(true);
+                    cam1static.SetActive(true);
+                    Cam2.gameObject.SetActive(false);
+                    currentlocation = "cam1";
+                    Cam1.gameObject.SetActive(true);
+                    yield return new WaitForSeconds(0.1f);
+                    cam2static.SetActive(false);
+                    cam1static.SetActive(false);
+                }
+                else if (currentlocation == "cam1")
+                {
+                    yield return new WaitForSeconds(8f);
+                    if (doorcam.activeSelf && !(backtoofficebutton.IsActive()))
+                    {
+                        Cam1.gameObject.SetActive(false);
+                        currentlocation = "hidden";
+                        gonegeluid.Play();
+                        Hidden.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        Cam1.gameObject.SetActive(false);
+                        shitdatindewegzit.SetActive(false);
+                        backtoofficebutton.gameObject.SetActive(false);
+                        currentlocation = "office";
+                        camerahandler.GetComponent<Cameras>().SwitchToCamDown(true);
+                        camerahandler.GetComponent<Cameras>().BackToTheOffice(true);
+                        jumpscareobject.gameObject.SetActive(true);
+                        jumpscare.Play();
+                        yield return new WaitForSeconds(1f);
+
+                        // Wait for a few seconds after the jumpscare before changing the scene
+                        // Change 3f to your desired delay
+
+                        // Load the next scene
+                        Application.Quit();
+                    }
+                }
+            }
+        }
+        // Start the coroutine again to continue its execution
+        StartCoroutine(UnknownMove());
+    }
+
+}
---
+++ Assets/scripts/Time/Night3Time.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Night3Time : MonoBehaviour
+{
+    public float gameDurationInSeconds = 60; // Total game duration in seconds
+    public float timeScale = 1.0f; // Adjustable speed of the timer
+    public GameObject textObject; // Reference to the GameObject containing TextMeshPro
+
+    private TextMeshProUGUI timerText;
+    private float currentTime;
+    private int currentHour = 0; // Track the current in-game hour
+    private bool gameEnded = false;
+
+    public RICK rickAI;
+    public KorsAI korsAI;
+    public PaulAI PaulAI;
+    public PeterAI PeterAI;
+
+    void Start()
+    {
+        currentTime = gameDurationInSeconds; // Start time set to the game duration
+
+        // Get the TextMeshPro component from the GameObject
+        timerText = textObject.GetComponent<TextMeshProUGUI>();
+        if (timerText == null)
+        {
+            Debug.LogError("No TextMeshProUGUI component found on the provided GameObject.");
+        }
+        UpdateTimerDisplay();
+    }
+
+    void Update()
+    {
+        if (!gameEnded)
+        {
+            currentTime -= Time.deltaTime * timeScale;
+
+            if (currentTime <= 0)
+            {
+                EndGame();
+            }
+            else
+            {
+                UpdateTimerDisplay();
+            }
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
+        float hoursPassed = (gameDurationInSeconds - currentTime) / (gameDurationInSeconds / 6); // Calculate hours passed
+        currentHour = Mathf.FloorToInt(hoursPassed);
+        string timeString = string.Format("{0} AM", (currentHour == 0) ? "12" : currentHour.ToString());
+
+        // Update the UI Text element
+        timerText.text = "" + timeString;
+
+        if (currentHour == 0)
+        {
+            rickAI.ailevel = 1;
+            korsAI.currentAILevel = 1;
+            PaulAI.ailevel = 1;
+            PeterAI.ailevel = 0;
+        }
+        else if (currentHour == 1)
+        {
+            rickAI.ailevel = 2;
+            korsAI.currentAILevel = 2;
+            PaulAI.ailevel = 2;
+            PeterAI.ailevel= 2;
+        }
+        else if(currentHour == 2)
+        {
+            rickAI.ailevel = 2;
+            korsAI.currentAILevel = 2;
+            PaulAI.ailevel = 2;
+            PeterAI.ailevel = 2;
+        }
+        else if (currentHour == 3)
+        {
+            rickAI.ailevel = 4;
+            korsAI.currentAILevel = 2;
+            PaulAI.ailevel = 2;
+            PeterAI.ailevel = 3;
+        }
+        else if (currentHour == 4)
+        {
+            rickAI.ailevel = 4;
+            korsAI.currentAILevel = 2;
+            PaulAI.ailevel = 3;
+            PeterAI.ailevel = 3;
+        }
+        else if (currentHour == 5)
+        {
+            rickAI.ailevel = 6;
+            korsAI.currentAILevel = 2;
+            PaulAI.ailevel = 3;
+            PeterAI.ailevel = 4;
+        }
+    }
+
+    void EndGame()
+    {
+        PlayerPrefs.SetString("currentnight", "Night 2");
+        gameEnded = true;
+        timerText.text = "6 AM";
+        SceneManager.LoadScene("6AM");
+    }
+
+    public void SetTimeScale(float newTimeScale)
+    {
+        timeScale = newTimeScale;
+    }
+}
